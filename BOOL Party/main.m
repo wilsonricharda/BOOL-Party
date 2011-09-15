3c4a2a85a157943e131a7a188e983f2d31b43470
Richard Wilson <Richie@Richard-Wilsons-MacBook.local>
2011-09-15 20:36:40 +0200

    Initial Commit

---
+++ BOOL Party/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BOOL Party
+//
+//  Created by Richard Wilson on 9/15/11.
+//  Copyright 2011 Imbarco, Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
