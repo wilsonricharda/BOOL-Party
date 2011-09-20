7a2858e1583358f9ba4e4b30fccaaa2e5cd30ba2
Richard Wilson <wilsonricharda@msn.com>
2011-09-21 01:05:20 +0200

    Initial commit
    Just a test of GitHub which I just installed.  It found some local
    repositories so I thought I would give it a go.

--- BOOL Party/main.m
+++ BOOL Party/main.m
@@ -8,15 +8,40 @@
 
 #import <Foundation/Foundation.h>
 
+//  returns NO if the two integers have the same
+//  value, YES otherwise
+
+BOOL areIntsDifferent (int thing1, int thing2)
+{
+    if (thing1 == thing2) {
+        return YES;
+    } else {
+        return (NO);
+    }
+}  // areIntsDifferent
+
+//  given a YES value, return the human-readable
+//  string "YES".  Otherwisde return "NO"
+
+NSString *boolString (BOOL yesNo)
+{
+    if (yesNo == YES) {
+        return (@"YES");
+    } else {
+        return (@"NO");
+    }
+}  // boolString
+
 int main (int argc, const char * argv[])
 {
+    BOOL areTheyDifferent;
+    
+    areTheyDifferent = areIntsDifferent(5, 5);
+    
+    NSLog(@"are %d and %d different? %@",
+          23, 42, boolString(areTheyDifferent));
+    
+    return (0);
 
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
-
-    [pool drain];
-    return 0;
-}
+}  // main
 
